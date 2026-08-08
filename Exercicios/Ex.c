e122b68c193169b792e1372fa86ba8c9fc09969c
Moises <moisesrangel97@gmail.com>
2026-08-08 07:51:52 -0300

    Desafio: nível aventureiro Movimentando o Cavalo

---
+++ Exercicios/Ex.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+int main (){
+    for(int i = 1; i <= 5; i++ ){
+        for(int j = 1; j <=5; j++){
+            printf("%d * %d = %d\n",i,j,i*j);
+        }
+        printf("\n");
+    }
+}*/
+
+/*
+int main (){
+    int triangulo = 10;
+    for(int i = 1; i <= triangulo; i++ ){
+        for(int j = 1; j <=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}*/
+
+int main () {
+    int segundos;
+
+    for(segundos = 10; segundos >=0; segundos--){
+        printf("%d\n", segundos);
+        for(int i = 0; i<1000000; i++){
+
+        }
+    }
+}
--- xadrez.c
+++ xadrez.c
@@ -5,26 +5,40 @@ int main() {
     // Definindo o número de movimentos possíveis para cada peça
     int torre = 5, bispo = 5, rainha = 8;
     int contadorBispo = 0, contadorRainha = 0;
-
+    printf("*************\n");
     // Movimentos da Torre
     printf("Movimento da Torre:\n");
     for(int contadorTorre = 0; contadorTorre < torre; contadorTorre++){
         printf("Direita\n");
     }
-
+    printf("*************\n");
     // Movimentos do Bispo
     printf("Movimento do Bispo:\n");
     while (contadorBispo < bispo){
         printf("Cima, Direita\n");
         contadorBispo++;
     }
-
+    printf("*************\n");
     // Movimentos da Rainha
     printf("Movimento da Rainha:\n");
     do {
         printf("Esquerda\n");
         contadorRainha++;
     } while (contadorRainha < rainha);
+    //Movimento do Cavalo
+    printf("*************\n");
+    int cavalo = 1;
+    for(int contadorCavalo = 0; contadorCavalo < cavalo; contadorCavalo++){
+        int contadorCavalo2 = 0;
+        while(contadorCavalo2 < 2){
+            printf("Baixo\n");
+            contadorCavalo2++;
+        }
+        printf("Esquerda\n");
+    }
+    printf("*************\n");
+
+
 
     return 0;
 }
